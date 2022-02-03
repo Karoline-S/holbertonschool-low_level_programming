a0c719b9deea6416fdb222f5866e5635199334b8
Karoline-S <karoline.amy@gmail.com>
2022-02-03 03:41:29 -0800

    swap 1 for zero in function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,7 @@ int main(void)
 
   char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-  fwrite(str, sizeof(str), stderr);
+  fwrite(str, 0,sizeof(str), stderr);
 
   return (1);
 
