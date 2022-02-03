4e9166592e465f1729809ada8b153933ea85bd9c
Karoline-S <karoline.amy@gmail.com>
2022-02-03 02:18:36 -0800

    Adv T9, first attempt

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the starting point, I think
+ *
+ * Return: This is where I describe what the return does
+ */
+int main(void)
+{
+
+  fprintf(stderr,
+	  NC "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return (0);
+
+}
