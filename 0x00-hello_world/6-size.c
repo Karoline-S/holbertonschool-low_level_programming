8613ef5d5647472f5b7e840c8a68d046e58077a5
Karoline-S <karoline.amy@gmail.com>
2022-02-03 01:21:50 -0800

    Task 6, first attempt

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the starting point, I think
+ *
+ * Return: This is where I describe what the return does
+ */
+int main(void)
+{
+printf("%lu\n", sizeof(char));
+printf("%lu\n", sizeof(int));
+printf("%lu\n", sizeof(long int));
+printf("%lu\n", sizeof(long long int));
+return (0);
+}
