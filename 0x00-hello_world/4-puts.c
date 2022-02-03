5c1feb05adc2a5090492b8567ed0161dc67837d1
Karoline-S <karoline.amy@gmail.com>
2022-02-02 23:37:42 -0800

    Task 4, first attempt

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+  /* initialising strings */
+char str1[] = "Programming is like building a multilingual puzzle"
+
+  /*writing to stdout */
+puts(str1);
+
+return (0);
+}
