3fd9473e374917470ab4d77c96e50b14a78a4c20
Karoline-S <karoline.amy@gmail.com>
2022-02-03 01:42:12 -0800

    Add additional text to output

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-printf("%lu\n", sizeof(char));
-printf("%lu\n", sizeof(int));
-printf("%lu\n", sizeof(long int));
-printf("%lu\n", sizeof(long long int));
+printf("Size of a char: \"%lu\n\" byte(s)", sizeof(char));
+printf("Size of a char: \"%lu\n\" byte(s)", sizeof(int));
+printf("Size of a char: \"%lu\n\" byte(s)", sizeof(long int));
+printf("Size of a char: \"%lu\n\" byte(s)", sizeof(long long int));
 return (0);
 }
