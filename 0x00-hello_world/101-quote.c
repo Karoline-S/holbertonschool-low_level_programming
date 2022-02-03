eb71adc0bc1b00e057407f33deded6e0dc895143
Karoline-S <karoline.amy@gmail.com>
2022-02-03 02:50:12 -0800

    Adv T9, use different command

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
+#define NC "/e[0m"
+
 /**
  * main - the starting point, I think
  *
  * Return: This is where I describe what the return does
  */
+
 int main(void)
 {
 
-  fprintf(stderr,
-	  NC "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-return (0);
+  fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62, 1, stderr);
+  exit(EXIT_FAILURE);
 
 }
