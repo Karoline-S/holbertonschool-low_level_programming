f1f6f940be7d53bd1f4788f983e37b485d1fe732
Karoline-S <karoline.amy@gmail.com>
2022-02-04 04:39:29 -0800

    Adv T10, change separator

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,6 +11,8 @@
 int main(void)
 {
 	int a1, a2;
+	int sep1 = 0;
+	int sep2 = 0;
 
 	a1 = '0';
 	while (a1 <= '9')
@@ -18,11 +20,13 @@ int main(void)
 		a2 = a1 + 1;
 		while (a2 <= '9')
 		{
+			putchar(sep1);
+			putchar(sep2);
 			putchar(a1);
 			putchar(a2);
-			putchar(',');
-			putchar(' ');
 			a2 = a2 + 1;
+			sep1 = ',';
+			sep2 = ' ';
 		}
 		a1 = a1 + 1;
 	}
