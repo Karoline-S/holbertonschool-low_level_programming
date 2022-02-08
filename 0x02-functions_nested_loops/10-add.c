76eb1e6470d72774dc9e9cd3f035fe13263da25e
Karoline-S <karoline.amy@gmail.com>
2022-02-08 02:08:36 -0800

    Add task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - short description here
+ *
+ * Description: long description here
+ *
+ * @a: describing the declared variable here
+ * @b: describing the declared variable here
+ *
+ * Return: explain what happens on return here
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
