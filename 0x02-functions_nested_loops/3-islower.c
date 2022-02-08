1134da8058c3987223f158ea3396970f31282a9a
Karoline-S <karoline.amy@gmail.com>
2022-02-07 21:04:47 -0800

    Add task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - short description here
+ *
+ * Description: long description here
+ *
+ *@c: describing here
+ *
+ * Return: explain what happens on return here
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+		_putchar(1);
+	else
+		_putchar(0);
+	return (0);
+}
