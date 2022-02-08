f1f00aec8713869ce31d486d82a4209aa3a14382
Karoline-S <karoline.amy@gmail.com>
2022-02-08 01:35:01 -0800

    Add task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_last_digit - short description here
+ *
+ * Description: long description here
+ *
+ * : describing the declared variable here
+ *
+ * Return: explain what happens on return here
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
