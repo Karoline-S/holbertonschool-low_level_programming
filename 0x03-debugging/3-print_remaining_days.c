c25ac1dae1615e2ce4620c5abfdd4f3bea117cc7
Karoline-S <karoline.amy@gmail.com>
2022-02-09 04:00:55 -0800

    Provide comprehensive invalid date arguments

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -27,6 +27,42 @@ void print_remaining_days(int month, int day, int year)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
+		else if (month == 3 && day >= 91)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else if (month == 4 && day >= 121)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else if (month == 5 && day >= 152)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else if (month == 6 && day >= 182)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else if (month == 7 && day >= 213)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else if (month == 8 && day >= 244)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else if (month == 9 && day >= 274)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else if (month == 10 && day >= 305)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else if (month == 11 && day >= 335)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
 		else
 		{
 			printf("Day of the year: %d\n", day);
