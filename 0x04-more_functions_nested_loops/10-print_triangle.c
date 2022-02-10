cbd20019dee05f226ce0ca0aa0de77d31dbf4496
Karoline-S <karoline.amy@gmail.com>
2022-02-10 01:20:04 -0800

    Add task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle with char
+ * size specified by input variable
+ * @size: automatic variable int
+ * Return: void.
+ */
+void print_triangle(int size)
+{
+	int space;
+	int hash;
+	int count = 1;
+
+	while (count <= size)
+	{
+		space = (size - count);
+		hash = (size - space);
+
+		while (space > 0)
+		{
+			_putchar(' ');
+			space = space - 1;
+		}
+		while (hash > 0)
+		{
+			_putchar('#');
+			hash = hash - 1;
+		}
+		_putchar('\n');
+		count = count + 1;
+	}
+}
