1a31bdca235cafce52d7a9931d610f7502b76aa5
Karoline-S <karoline.amy@gmail.com>
2022-02-10 00:21:22 -0800

    Add task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0-100 except numbers divisible by 3 and/or
+ * 5 print word
+ * Return: void
+ */
+int main(void)
+{
+	int n = 1;
+
+	while (n < 101)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FizzBuzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
+		else if (n % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", n);
+		n = n + 1;
+	}
+	printf("\n");
+	return (0);
+}
