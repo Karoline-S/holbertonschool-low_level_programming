54730cb59dd0e408a6d08c592a39e142f43a6867
Karoline-S <karoline.amy@gmail.com>
2022-02-09 16:58:35 -0800

    Add task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check if variable is a digit
+ * @a: automatic variable int
+ * @b: automatic variable int
+ * Return: Always 1 if digit, 0 otherwise.
+ */
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+	return (x);
+}
