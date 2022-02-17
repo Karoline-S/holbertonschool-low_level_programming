96a1d0c11591af89425461077a09813fb162bad1
Karoline-S <karoline.amy@gmail.com>
2022-02-17 03:44:31 -0800

    Use string pointers to compare

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,39 +1,29 @@
 #include "main.h"
 
 /**
- * _codeSwap - swaps the char by providing a new ascii value
- *@a: ascii value int to be swapped
- * Return: nothing
- */
-int _codeSwap(int a)
-{
-	if (a == 97 || a == 65)
-		a = 52;
-	else if (a == 101 || a == 69)
-		a = 51;
-	else if (a == 111 || a == 79)
-		a = 48;
-	else if (a == 116 || a == 84)
-		a = 55;
-	else if (a == 108 || a == 76)
-		a = 49;
-	return (a);
-}
-
-/**
  * *leet - encodes a string) a/A=4, e/E=3, o/O=0, t/T=7, l/L=1.
- * @str: the string to be altered
+ * @str: the string to be altered1
  * charFound = 1 when a char matches a coded char and 0 when not
  * Return: a pointer to the altered string.
  */
 char *leet(char *str)
 {
-	int count = 0;
+	char *alpha = "aAeEoOtTlL";
+	char *num = "4433007711";
+	int counta = 0;
+	int countb;
 
-	while (str[count] != '\0')
+	while (str[counta] != '\0')
 	{
-		str[count] = _codeSwap(str[count]);
-		count++;
+		countb = 0;
+
+		while (alpha[countb] != '\0')
+		{
+			if (str[counta] == alpha[countb])
+				str[counta] = num[countb];
+			countb++;
+		}
+		counta++;
 	}
 	return (str);
 }
