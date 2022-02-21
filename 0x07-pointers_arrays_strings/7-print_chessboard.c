04c35d0b9aa22dbe0c9574a5e8498a628482ccad
Karoline-S <karoline.amy@gmail.com>
2022-02-21 00:38:33 -0800

    Remove stdio.h and testing lines

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_chessboard - prints an array of chars as a chessboard
@@ -8,23 +7,6 @@
  */
 void print_chessboard(char (*a)[8])
 {
-/*	int row = 0;
-	int col = 0;
-
-	_putchar(a[row][col]);
-
-	row = 1;
-	col = 6;
-	printf("%c\n", a[row][col]);
-
-	row = 5;
-	col = 2;
-	printf("%c\n", a[row][col]);
-
-	row = 7;
-	col = 7;
-	printf("%c\n", a[row][col]);
-*/
 	int row = 0;
 	int col;
 
