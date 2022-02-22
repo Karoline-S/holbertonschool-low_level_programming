800939bb118bc32757676c90d651225add0d269c
Karoline-S <karoline.amy@gmail.com>
2022-02-22 13:52:35 -0800

    Change position of new line statement

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
+		_putchar('\n');
+	else if (*s != '\0')
 	{
 		_putchar(*s);
 		s++;
 		_puts_recursion(s);
 	}
-	if (*s == '\0')
-		_putchar('\n');
 }
