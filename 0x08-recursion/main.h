6e31e95c8f19220c6ab5da6cda83e51fc6340b30
Karoline-S <karoline.amy@gmail.com>
2022-02-22 12:06:06 -0800

    Correct syntax of main file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,5 @@
-#IFNDEF MAIN_H
-#DEFINE MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 void _putchar(char);
 void _puts_recursion(char *s);
@@ -12,4 +12,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
-#ENDIF /* MAIN_H */
+#endif /* MAIN_H */
