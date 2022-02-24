ed5a796c2970a56151d509bdc07f54145dcdbd50
Karoline-S <karoline.amy@gmail.com>
2022-02-24 01:22:21 -0800

    Add adv task 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,36 +2,21 @@
 #include "main.h"
 
 /**
- * print_sum_multiples - prints the sum of all mutliples of 3 or 5 up to a sum
- * less than 1024.
+ * main - computes and prints the sum of multiples of 3 or 5 up to and
+ * excluding 1024.
  * Return: nothing
- */
-void print_sum_multiples(int n)
-{
-	int a = 0;
-	int result;
-	int sum = 0;
-
-	while (sum < 950)
-	{
-		result = a * n;
-		sum = sum + result;
-		if (sum == 0)
-			printf("%d", sum);
-		else
-			printf(", %d", sum);
-		a++;
-	}
-	printf("\n");
-}
-
-/**
- * main - unsure how to describe this
- * Return: 0 for success.
  */
 int main(void)
 {
-	print_sum_multiples(3);
-	print_sum_multiples(5);
+	int sum = 0;
+	int multi = 3;
+
+	while (multi < 1024)
+	{
+		if (multi % 3 == 0 || multi % 5 == 0)
+			sum = sum + multi;
+		multi++;
+	}
+	printf("%d\n", sum);
 	return (0);
 }
