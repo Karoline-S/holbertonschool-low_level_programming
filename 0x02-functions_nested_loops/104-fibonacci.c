803d3d78ab81add4ea7eb4e801c9ef4672c967e3
Karoline-S <karoline.amy@gmail.com>
2022-02-24 00:53:53 -0800

    Add adv task 104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers to stdout
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int count = 4;
+	long double fib1 = 1;
+	long double fib2 = 2;
+	long double fibPlus;
+
+	printf("%.0Lf", fib1);
+	printf(", %.0Lf", fib2);
+	while (count < 99)
+	{
+		fibPlus = fib1 + fib2;
+		printf(", %.0Lf", fibPlus);
+		fib1 = fib2;
+		fib2 = fibPlus;
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
