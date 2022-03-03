fcd003c1a5058f6e49a03222923c1c077f290f2f
Karoline-S <karoline.amy@gmail.com>
2022-03-02 21:52:40 -0800

    fix - remove small functions and fill string in nconcat function to avoid overuse of memory

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,7 +5,7 @@
  * @s: pointer to the string
  * Return: length of string
  */
-int _strlen(char *s)
+unsigned int _strlen(char *s)
 {
 	if (*s == '\0')
 		return (0);
@@ -13,52 +13,53 @@ int _strlen(char *s)
 	return (1 + _strlen(s + 1));
 }
 
-/**
+/*
  * _strcpy - copies string at src into memory at dest
  * @dest: pointer to the space for string
  * @src: pointer to string to be copied
  * Return: pointer to dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	unsigned int i;
-
-	if (*src == '\0')
-		return (dest);
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-/**
+ *
+ *char *_strcpy(char *dest, char *src)
+ *{
+ *	unsigned int i;
+ *
+ *	if (*src == '\0')
+ *		return (dest);
+ *	i = 0;
+ *	while (src[i] != '\0')
+ *	{
+ *		dest[i] = src[i];
+ *		i++;
+ *	}
+ *	dest[i] = '\0';
+ *	return (dest);
+ *}
+ *
+ *
  * _strncat - appends string at src to the end of string at dest
  * @dest: - pointer to the destination string
  * @src: - pointer to the copy string
  * @n: - the number of bytes (at most) to copy from src
  * Return: pointer to dest
+ *
+ *char *_strncat(char *dest, char *src, unsigned int n)
+ *{
+ *	unsigned int destlen, i;
+ *
+ *	destlen = _strlen(dest);
+ *
+ *	i = 0;
+ *	while (src[i] != '\0' && n > 0)
+ *	{
+ *		dest[destlen] = src[i];
+ *		destlen++;
+ *		i++;
+ *		n--;
+ *	}
+ *	dest[destlen] = '\0';
+ *	return (dest);
+ *}
  */
-char *_strncat(char *dest, char *src, unsigned int n)
-{
-	unsigned int destlen, i;
-
-	destlen = _strlen(dest);
-
-	i = 0;
-	while (src[i] != '\0' && n > 0)
-	{
-		dest[destlen] = src[i];
-		destlen++;
-		i++;
-		n--;
-	}
-	dest[destlen] = '\0';
-	return (dest);
-}
 
 /**
  * string_nconcat - concatenates 2 strings in newly created malloc memory
@@ -70,7 +71,7 @@ char *_strncat(char *dest, char *src, unsigned int n)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *newStr;
-	unsigned int length1, length2;
+	unsigned int length1, length2, i, ii;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -89,9 +90,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (newStr == NULL)
 		return (NULL);
 
-	newStr = _strcpy(newStr, s1);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		newStr[i] = s1[i];
+		i++;
+	}
 
-	newStr = _strncat(newStr, s2, n);
-
+	ii = 0;
+	while (n > 0)
+	{
+		newStr[i] = s2[ii];
+		i++;
+		ii++;
+		n--;
+	}
+	newStr[i] = '\0';
+/*	newStr = _strcpy(newStr, s1);
+ *
+ *	newStr = _strncat(newStr, s2, n);
+ */
 	return (newStr);
 }
