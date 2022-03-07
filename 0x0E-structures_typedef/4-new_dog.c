7c9e4d6323332d37d2adadfc713bd66249abad65
Karoline-S <karoline.amy@gmail.com>
2022-03-06 19:10:02 -0800

    perf - copy strings to new memory

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,27 @@
 #include "dog.h"
 
 /**
+ * _strcpy - copies the string at src to the memory ta dest
+ * @dest: pointer to the memory for the new string
+ * @src: pointer to the string to copy
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
  * new_dog - creates a new variable from struct dog
  * @name: value to be stored at .name
  * @age: value to be stored at .age
@@ -12,25 +33,28 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
+	char *sName, *sOwner;
 
 	new = malloc(sizeof(dog_t));
 
 	if (new == NULL)
 		return (NULL);
 
-	new->name = name;
+	sName = malloc(sizeof(*name));
+
+	if (sName != NULL)
+		sName = _strcpy(sName, name);
+
+	sOwner = malloc(sizeof(*owner));
+
+	if (sOwner != NULL)
+		sOwner = _strcpy(sOwner, owner);
+
+	new->name = sName;
 	new->age = age;
-	new->owner = owner;
+	new->owner = sOwner;
 
-	new->name = malloc(sizeof(*name));
-
-	if (new->name != NULL)
-		*(new->name) = *name;
-
-	new->owner = malloc(sizeof(*owner));
-
-	if (new->owner != NULL)
-		*(new->owner) = *owner;
+	printf("sName string is: %s\n", sName);
 
 	return (new);
 }
