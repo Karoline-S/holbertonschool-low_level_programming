beeb43a8bf1693d050a0ec4ccdefb75743c174d2
Karoline-S <karoline.amy@gmail.com>
2022-03-08 12:12:32 -0800

    build - add task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - calls back a function via a pointer
+ * @name: the name for function argument
+ * @f: the function to be called.
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+	return;
+}
