ab23c9a1f59bf35d9cf5b966686fa18a45ac8343
Karoline-S <karoline.amy@gmail.com>
2022-03-09 00:37:27 -0800

    fix - change argv syntax at line 20, remove whitespace in header file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,32 +9,32 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2;
-	int (*calc)(int, int);
+	int (*f)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 
-	if (argv[3] == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+	if (argv[3][0] == '0' && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 
 
-	calc = get_op_func(argv[2]);
+	f = get_op_func(argv[2]);
 
-	if (calc == NULL)
+	if (f == NULL)
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	printf("%d\n", calc(num1, num2));
+	printf("%d\n", f(num1, num2));
 
 	return (0);
 }
