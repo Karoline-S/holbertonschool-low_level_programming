f185ff6e0a5b3f50a975888c862c1398587e3dc7
Karoline-S <karoline.amy@gmail.com>
2022-03-09 17:25:55 -0800

    test - use va_copy for null string test in while loop

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,25 +9,30 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list ap;
+	va_list ap1;
+	va_list ap2;
 	unsigned int i;
 
 	va_start(ap, n);
+	va_copy(ap2, ap1);
 
 	i = 0;
 
 	while (i < n)
 	{
-		if (va_arg(ap, char *) == NULL)
+		if (va_arg(ap1 == NULL))
 			printf("nil");
 
 		if (i != 0 && separator != NULL)
-			printf("%s%s", separator, va_arg(ap, char *));
+			printf("%s%s", separator, va_arg(ap2, char *));
 
 		else
-			printf("%s", va_arg(ap, char *));
+			printf("%s", va_arg(ap2, char *));
 
 		i++;
 	}
 	printf("\n");
+
+	va_end(ap1);
+	va_end(ap2);
 }
