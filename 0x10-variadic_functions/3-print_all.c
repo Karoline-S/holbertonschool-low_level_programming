f9a8a8af6f00c29c248a96aeab7dd2530581222b
Karoline-S <karoline.amy@gmail.com>
2022-03-10 00:15:40 -0800

    change new line print conditions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,13 +8,11 @@
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	int idx;
-	char *string;
-	char *separator = "";
+	int idx = 0, count = 0;
+	char *string, *separator = "";
 
 	if (format == NULL)
 		return;
-
 	va_start(ap, format);
 	idx = 0;
 	while (format[idx] != '\0')
@@ -24,14 +22,17 @@ void print_all(const char * const format, ...)
 		case 'c':
 			printf("%s%c", separator, va_arg(ap, int));
 			separator = ", ";
+			count++;
 			break;
 		case 'i':
 			printf("%s%d", separator, va_arg(ap, int));
 			separator = ", ";
+			count++;
 			break;
 		case 'f':
 			printf("%s%f", separator, va_arg(ap, double));
 			separator = ", ";
+			count++;
 			break;
 		case 's':
 			string = va_arg(ap, char *);
@@ -39,14 +40,21 @@ void print_all(const char * const format, ...)
 			{
 				printf("%s%s", separator, string);
 				separator = ", ";
+				count++;
 				break;
 			}
 			printf("%snil", separator);
 			separator = ", ";
+			count++;
 			break;
 		}
 		idx++;
 	}
 	va_end(ap);
-	printf("\n");
+
+	while (count != 0)
+	{
+		printf("\n");
+		break;
+	}
 }
