820d32515ad26135e1ce1bc03a7ca246ec86c663
Karoline-S <karoline.amy@gmail.com>
2022-03-16 02:57:45 -0700

    perf - add edge case output change

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,14 @@ size_t print_list(const list_t *h)
 {
 	if (h == NULL)
 		return (0);
-	printf("[%d] %s\n", h->len, h->str);
-	h = h->next;;
+
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+
+	else
+		printf("[%d] %s\n", h->len, h->str);
+
+	h = h->next;
+
 	return (1 + print_list(h));
 }
