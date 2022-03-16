366f3a4f30d9b536fbe6f29563187094e400f2dd
Karoline-S <karoline.amy@gmail.com>
2022-03-16 02:51:48 -0700

    build - add task 0 and header file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head node
+ * Return: number of nodes as size_t
+ *
+ */
+size_t print_list(const list_t *h)
+{
+	if (h == NULL)
+		return (0);
+	printf("[%d] %s\n", h->len, h->str);
+	h = h->next;;
+	return (1 + print_list(h));
+}
