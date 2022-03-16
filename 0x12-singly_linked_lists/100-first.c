8f0821b8979088555f46effba53437bb72698e37
Karoline-S <karoline.amy@gmail.com>
2022-03-16 05:33:01 -0700

    build - add adv task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_before_main - prints a string before main is called
+ * Return: nothing
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
