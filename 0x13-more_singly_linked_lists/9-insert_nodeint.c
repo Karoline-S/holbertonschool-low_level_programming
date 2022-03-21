039e316d47e9fdfe15ddd7f7a33a0c58fc9a4f01
Karoline-S <karoline.amy@gmail.com>
2022-03-21 05:12:12 -0700

    perf - delete unneeded code from task 3

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -56,49 +56,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	return (new);
 }
 
-/*
- * add_nodeint_end - adds a new list_t element at the end
- * @head: a pointer to a pointer to the first node
- * @n: the integer to be held in the new node
- * Return: the address of the new node
- *
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-	listint_t *new, *hold;
-
-	if (head == NULL)
-		return (NULL);
-
-	new = malloc(sizeof(*new));
-
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = NULL;
-
-	if (*head == NULL)
-	{
-		*head = new;
-		return (*head);
-	}
-	else
-	{
-		hold = *head;
-		while (hold != NULL)
-		{
-			if (hold->next == NULL)
-			{
-				hold->next = new;
-				return (*head);
-			}
-			hold = hold->next;
-		}
-	}
-
-	return (new);
-}
-*/
 /**
  * insert_nodeint_at_index - insert new node at nth node point
  * @head: pointer to pointer to start of the list
