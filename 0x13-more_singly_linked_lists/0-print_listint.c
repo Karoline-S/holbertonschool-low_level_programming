7383b353382ab969a75af8d6f69c71dc66d92245
Karoline-S <karoline.amy@gmail.com>
2022-03-20 17:21:09 -0700

    perf - recode using recursion

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,16 +9,11 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
-
 	if (h == NULL)
 		return (0);
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
-	}
-	return (count);
+	printf("%d\n", h->n);
+	h = h->next;
+
+	return (1 + print_listint(h));
 }
