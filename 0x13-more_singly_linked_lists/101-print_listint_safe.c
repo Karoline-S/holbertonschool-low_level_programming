68011c5623fb2dc37e8df93659037fee145ab7e5
Karoline-S <karoline.amy@gmail.com>
2022-03-22 14:22:29 -0700

    test - change exit(98) to return (0)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,6 +3,51 @@
 #include "lists.h"
 
 /**
+ * add_node - adds a new node at the beginning of a list
+ * @head: a pointer to a pointer to the start of the list
+ * @n: the integer to be inserted in the new node
+ * Return: address of the new element
+ */
+ptrhold *add_node(ptrhold **head, listint_t *hold)
+{
+	ptrhold *new;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(*new));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->hold = hold;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
+
+/*
+ * free_listint_s - frees a ptrhold list
+ * @head: a pointer to the start of the list
+ * Return: nothing.
+ *
+void free_list_s(ptrhold *head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
+*/
+/**
  * print_listint_safe - prints a list with our without a loop, identifies loop
  * elements
  * @head: a pointer to the start of the list
@@ -15,14 +60,13 @@ size_t print_listint_safe(const listint_t *head)
 	int count = 0;
 
 	if (head == NULL)
-		exit(98);
+		return (0);;
 
-	newhead = malloc(sizeof(*newhead));
-	if (newhead == NULL)
-		exit(98);
 	temphead = (listint_t *)head;
-	newhead->hold = temphead;
-	newhead->next = NULL;
+	newhead = NULL;
+	add_node(&newhead, temphead);
+	if (newhead == NULL)
+		return (0);
 	temp = newhead;
 	newheadCpy = newhead;
 	while (temphead != NULL)
@@ -30,7 +74,7 @@ size_t print_listint_safe(const listint_t *head)
 		printf("[%p] %d\n", (void *)temphead, temphead->n);
 		newhead->next = malloc(sizeof(*newhead));
 		if (newhead->next == NULL)
-			exit(98);
+			return (0);
 		newhead = newhead->next;
 		newhead->hold = temphead->next;
 		newhead->next = NULL;
