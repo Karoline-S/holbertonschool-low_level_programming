453524b989c577634060dd961a57c0c0ac77225c
Karoline-S <karoline.amy@gmail.com>
2022-03-24 05:03:51 -0700

    build - add task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - count the number of bits to be flipped to get
+ * from one number to another
+ * @n: the base number
+ * @m: the target number
+ * Return: the number of bits to be flipped
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int count = 0;
+	int x = n ^ m;
+
+	while (x != 0)
+	{
+		count = count + (x & 1);
+		x = x >> 1;
+	}
+
+	return (count);
+}
