23aa5845f46388a164bc56fc2c99b86b02f8f629
Karoline-S <karoline.amy@gmail.com>
2022-03-24 05:14:10 -0700

    fix - adjust while loop condition to be greater than 0

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,10 +12,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int count = 0;
 	int x = n ^ m;
 
-	while (x != 0)
+	while (x > 0)
 	{
 		count = count + (x & 1);
-		x >>= 1;
+		x = x >> 1;
 	}
 
 	return (count);
