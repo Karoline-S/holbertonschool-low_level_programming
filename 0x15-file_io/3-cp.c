5620584a7ff666672d71f751dad8176eb4e78a79
Karoline-S <karoline.amy@gmail.com>
2022-03-28 19:05:51 -0700

    fix - incorrect count of chars on first error check

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -130,7 +130,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		write(STDERR_FILENO, "Usage: cp file_from file_to", 29);
+		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
 		exit(97);
 	}
 
