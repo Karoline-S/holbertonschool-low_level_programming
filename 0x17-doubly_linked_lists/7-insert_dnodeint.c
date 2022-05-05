e5525dd79deebeb3ab9d80a361d9a41cbcd6f0b4
Karoline-S <karoline.amy@gmai.com>
2022-05-04 19:55:53 -0700

    fix - update prev for node ahead when added to middle of list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -94,6 +94,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	new->prev = hold->prev;
 	new->next = hold;
+	new->next->prev = new;
 
 	return (new);
 }
